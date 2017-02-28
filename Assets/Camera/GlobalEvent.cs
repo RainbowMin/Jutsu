76a49eac4d02f54d2aac7e45a1d6be19dcbca252
Shinao <rmonnerat.pro@gmail.com>
2017-02-28 14:09:14 +0100

    Camera assets

---
+++ Assets/Camera/GlobalEvent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalEvent : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// UnityEditor.SceneView.FocusWindowIfItsOpen(typeof(UnityEditor.SceneView));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.P) && Input.GetKey(KeyCode.RightShift))
+			Debug.Break();
+	}
+}
